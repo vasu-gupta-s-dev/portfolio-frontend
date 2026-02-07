a7f9290dc89e18c8d6b8008502903e9b284f1104
vasug369 <vasupankajdbd@gmail.com>
2026-02-07 19:01:16 +0530

    Feature: Add resume download button to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,19 @@ const Home = () => {
                                     <path d="M5 12h14M12 5l7 7-7 7" />
                                 </svg>
                             </Link>
-                            <Link to="/contact" className="btn btn-secondary">
+                            <a
+                                href="/resume.pdf"
+                                download="Vasu_Gupta_Resume.pdf"
+                                className="btn btn-secondary"
+                            >
+                                <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" className="btn-icon">
+                                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                                    <polyline points="7,10 12,15 17,10" />
+                                    <line x1="12" y1="15" x2="12" y2="3" />
+                                </svg>
+                                Resume
+                            </a>
+                            <Link to="/contact" className="btn btn-outline">
                                 Get in Touch
                             </Link>
                         </div>
